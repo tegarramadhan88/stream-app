53fe7fa1940e251d08e566a646682a34368c83ad
tegar ramadhan <tegar.ramadhan@gmail.com>
2022-12-28 13:26:27 +0700

    selesai page admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MovieController;
 use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Admin\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,14 @@ use App\Http\Controllers\Admin\TransactionController;
 |
 */
 
-Route::group(['prefix' => 'admin'], function() {
+Route::get('admin/login', [LoginController::class, 'index'])->name('admin.login');
+Route::post('admin/login', [LoginController::class, 'authenticate'])->name('admin.login.auth');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['admin.auth']], function() {
     Route::view('/', 'admin.dashboard')->name('admin.dashboard');
 
+    Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
+    
     Route::get('/transaction', [TransactionController::class, 'index'])->name('admin.transaction');
 
     Route::group(['prefix' => 'movie'], function() {
